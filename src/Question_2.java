91c282da0bf25c775d2f5734d4cc5eb0cc3b0021
Vijeta Mankar <vijetamankar14@gmail.com>
2023-08-16 14:48:30 +0530

    Print Integers

---
+++ src/Question_2.java
@@ -0,0 +1,9 @@
+//Print integer in java from 1 to 10
+
+public class Question_2 {
+    public static void main(String[] args) {
+        for (int i =1 ;i<=10; i++){
+            System.out.println(i);
+        }
+    }
+}
