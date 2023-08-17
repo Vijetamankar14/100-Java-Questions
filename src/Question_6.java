316dcd73972afbadf5ced5d8f757240ccb137a1b
Vijeta Mankar <vijetamankar14@gmail.com>
2023-08-17 13:20:41 +0530

    Swapping

---
+++ src/Question_6.java
@@ -0,0 +1,16 @@
+//Swap two number using third variable
+
+public class Question_6 {
+    public static void main(String[] args) {
+        int a =20;
+        int b = 30;
+        int temp;
+        System.out.println(a+" "+b);
+        temp = a;
+        a=b;
+        b= temp;
+        System.out.println(a+" "+b);
+
+
+    }
+}
