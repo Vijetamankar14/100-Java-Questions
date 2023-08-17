b9d945b76851f5bf36276f8ca40a48a56ed09857
Vijeta Mankar <vijetamankar14@gmail.com>
2023-08-17 14:47:44 +0530

    If-else Clause

--- src/Question_10.java
+++ src/Question_10.java
@@ -2,14 +2,13 @@
 
 
 public class Question_10 {
-        public static void main(String[] args) {
-            boolean learning = false;
-            if (learning) {
-                System.out.println("Java programmer");
-            }
-            else {
-                System.out.println("What are you doing here?");
-            }
+    public static void main(String[] args) {
+        boolean learning = true;
+        if (learning) {
+            System.out.println("Java programmer");
+        }
+        else {
+            System.out.println("What are you doing here?");
         }
     }
-
+}
