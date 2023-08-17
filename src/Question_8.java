e842ea5204be818c75166fa9c9f9df9007834412
Vijeta Mankar <vijetamankar14@gmail.com>
2023-08-17 13:34:52 +0530

    Sum of two no

---
+++ src/Question_8.java
@@ -0,0 +1,19 @@
+//How to add two number
+
+import java.util.Scanner;
+
+public class Question_8 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter two numbers ");
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+
+        int c = a+b;
+
+        System.out.println("The Sum of two numbers is : "+c);
+
+
+
+    }
+}
