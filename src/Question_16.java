0ab27df8decab9695ed6e8edfcef891a58a4f4cf
Vijeta Mankar <vijetamankar14@gmail.com>
2023-08-19 14:50:34 +0530

    Loop

---
+++ src/Question_16.java
@@ -0,0 +1,10 @@
+//Simple For Loop Program in Java
+//Java for loop program
+class ForLoop {
+    public static void main(String[] args) {
+        int c;
+        for (c = 1; c <= 10; c++) {
+            System.out.println(c);
+        }
+    }
+}
