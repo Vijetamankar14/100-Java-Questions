ccd1766c3212f42d248f521df07afdc64e945c82
Vijeta Mankar <vijetamankar14@gmail.com>
2023-08-19 14:59:22 +0530

    Decrement Star Pattern

---
+++ src/Question_18.java
@@ -0,0 +1,13 @@
+public class Question_18 {
+
+
+        public static void main(String[] args) {
+            int row, numberOfStars;
+            for (row = 1; row <= 10; row++) {
+                for(numberOfStars = 10; numberOfStars >= row; numberOfStars--) {
+                    System.out.print("*");
+                }
+                System.out.println(); // Go to next line
+            }
+        }
+    }
