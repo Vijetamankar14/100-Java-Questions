5e02e754fa8b406197e66e09013cfd15f77ffb86
Vijeta Mankar <vijetamankar14@gmail.com>
2024-08-11 00:11:19 +0530

    Switch case

---
+++ src/Conditional_Switch.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Conditional_Switch {
+    public static void main(String args[]){
+
+        System.out.print("Enter any number b/w 1 to 3 : ");
+        Scanner scan = new Scanner(System.in);
+        int a= scan.nextInt();
+
+        switch (a){
+            case 1:
+                System.out.println("Hello");
+                break;
+            case 2:
+                System.out.println("Namaste");
+                break;
+            case 3:
+                System.out.println("Bonjour");
+                break;
+            default:
+                System.out.println("Welcome...");
+        }
+    }
+}
