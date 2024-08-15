ae9728048abefbaadbc4912800980bdb3861eb1e
Vijeta Mankar <vijetamankar14@gmail.com>
2024-08-15 15:00:04 +0530

    Star patterns 5

---
+++ src/Exercise5_Loops.java
@@ -0,0 +1,15 @@
+public class Exercise5_Loops {
+    public static void main(String args[]){
+        for (int i =1; i<=4; i++){
+            for (int j =1 ; j<=4-i;j++){
+                System.out.print(" ");
+            }
+            for (int k =1 ; k<=i ; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+            }
+
+        }
+    }
+
