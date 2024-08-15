22a98bfacf3708515aa4c3ec7a8d332f6af0900e
Vijeta Mankar <vijetamankar14@gmail.com>
2024-08-15 15:48:42 +0530

    Star patterns 8

---
+++ src/Exercise8_Loops.java
@@ -0,0 +1,16 @@
+public class Exercise8_Loops {
+    public static void main(String agrs[]){
+        int n =1;
+        for (int i =1; i<=5; i++){
+            for (int j=1; j<=i; j++){
+
+
+
+                System.out.print(n+" ");
+                n++;
+            }
+
+            System.out.println();
+        }
+    }
+}
