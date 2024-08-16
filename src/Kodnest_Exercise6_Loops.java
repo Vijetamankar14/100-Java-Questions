fa53ffee3b87466a8f5164aa72097b228ec5b6d2
Vijeta Mankar <vijetamankar14@gmail.com>
2024-08-16 14:19:35 +0530

    Loops patterns 6

---
+++ src/Kodnest_Exercise6_Loops.java
@@ -0,0 +1,10 @@
+public class Kodnest_Exercise6_Loops {
+    public static void main(String args[]){
+        for (int i=1 ; i<=5; i++){
+            for (int j =1 ; j<=5 ; j++){
+                System.out.print("$ ");
+            }
+            System.out.println();
+        }
+    }
+}
