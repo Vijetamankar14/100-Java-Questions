29b27c72625bb89cd9aab8a611f1e74a08864226
Vijeta Mankar <vijetamankar14@gmail.com>
2024-08-16 15:55:20 +0530

    Loops patterns 12,13,14

---
+++ src/Kodnest_Exercise13_Loops.java
@@ -0,0 +1,14 @@
+public class Kodnest_Exercise13_Loops {
+    public static void main(String args[]){
+        for(int i =1; i<=5; i++){
+            for (int j =1; j<=i; j++){
+                System.out.print("_");
+            }
+            for (int k =1 ; k<=5; k++){
+                System.out.print("$");
+            }
+            System.out.println( );
+        }
+    }
+}
+
---
+++ src/Kodnest_Exercise14_Loops.java
@@ -0,0 +1,14 @@
+public class Kodnest_Exercise14_Loops {
+    public static void main(String args[])
+    {
+        for (int i =1; i<=5; i++){
+            for (int j =5 ; j>=i;j--){
+                System.out.print(" ");
+            }
+            for(int k =1; k<=5;k++){
+                System.out.print("$"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
