cc13bd6565421c7791b7ca2ba7f22f5fab979126
Vijeta Mankar <vijetamankar14@gmail.com>
2024-08-21 22:54:30 +0530

    Loops Homework 4,5

---
+++ src/ExerciseHW4_Loops.java
@@ -0,0 +1,10 @@
+public class ExerciseHW4_Loops {
+    public static void main(String args[]){
+        for(int i =1 ; i<=5;i++){
+            for (int j =1 ;j <=i ; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
