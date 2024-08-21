065de572fb3fe17cf7a04c6a6e5806de33da925b
Vijeta Mankar <vijetamankar14@gmail.com>
2024-08-21 22:42:41 +0530

    Loops Homework 2

---
+++ src/ExerciseHW2_Loops.java
@@ -0,0 +1,10 @@
+public class ExerciseHW2_Loops {
+    public static void main(String args[]){
+        for (int i =1; i<=4; i++){
+            for (int j =4 ; j>=i; j--){
+                System.out.print("*" +" ");
+            }
+            System.out.println();
+        }
+    }
+}
